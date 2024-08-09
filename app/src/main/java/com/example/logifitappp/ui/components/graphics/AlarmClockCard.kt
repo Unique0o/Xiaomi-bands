49598b781d2341b487ac3cf607da4f9cac599329
MichaelSoro <maikolsoro.z1998@gmail.com>
2024-08-08 19:38:07 -0600

    Add Card y components graphics

---
+++ app/src/main/java/com/example/logifitappp/ui/components/graphics/AlarmClockCard.kt
@@ -0,0 +1,98 @@
+package com.example.logifitappp.ui.components.graphics
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.logifitappp.R
+import com.example.logifitappp.ui.components.home.ConnectedIndicator
+
+
+@Composable
+fun AlarmClockCard(modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFF0F4F8)),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_alarm),
+                        contentDescription = stringResource(id = R.string.content_description_alarm),
+                        tint = Color.Blue
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = stringResource(id = R.string.wake_up),
+                        style = MaterialTheme.typography.labelSmall,
+                        color = Color.Blue
+                    )
+                }
+                ConnectedIndicator(text = "20 min", Color(0xFF4CAF50))
+            }
+
+            AlarmRow(duration = "5min", startTime = "02:14", endTime = "02:19")
+            AlarmRow(duration = "15min", startTime = "03:54", endTime = "04:09")
+
+        }
+    }
+}
+
+@Composable
+fun AlarmRow(
+    duration: String,
+    startTime: String,
+    endTime: String
+) {
+    Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = duration,
+                style = MaterialTheme.typography.bodySmall,
+
+            )
+        }
+        ProgressBar(color = Color(0xFFFFB300))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = startTime,
+                style = MaterialTheme.typography.bodySmall,
+
+                )
+            Text(
+                text = endTime,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(end = 16.dp)
+            )
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/logifitappp/ui/screens/graphics/GraphicScreen.kt
@@ -0,0 +1,368 @@
+package com.example.logifitappp.ui.screens.graphics
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.logifitappp.R
+import com.example.logifitappp.ui.components.graphics.AlarmClockCard
+import com.example.logifitappp.ui.theme.LogifitApppTheme
+
+@Composable
+fun MainScreen1() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF5F5F5))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+//        UserHeader()
+//        Spacer(modifier = Modifier.height(16.dp))
+//        SleepMetricsCard()
+//        Spacer(modifier = Modifier.height(16.dp))
+//        StepsMetricsCard()
+//        Spacer(modifier = Modifier.height(16.dp))
+       AlarmClockCard(modifier = Modifier.padding(16.dp))
+        }
+    }
+}
+
+
+@Composable
+fun UserHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_pic),
+            contentDescription = "Foto de perfil",
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Column(modifier = Modifier.padding(start = 16.dp)) {
+            Text(
+                text = "Bienvenido de vuelta",
+//                style = MaterialTheme.typography.caption,
+                color = Color.Gray
+            )
+            Text(
+                text = "MARIA MERCEDES",
+//                style = MaterialTheme.typography.h6,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF2E5B9A)
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_star),
+                    contentDescription = null,
+                    tint = Color(0xFF2196F3),
+                    modifier = Modifier.size(16.dp)
+                )
+                Text(
+                    text = "PREMIUM",
+//                    style = MaterialTheme.typography.caption,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF2196F3),
+                    modifier = Modifier.padding(start = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MetricsCard(
+    title: String,
+    content: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFF5F5F5)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 0.dp),
+
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = title,
+//                style = MaterialTheme.typography.subtitle1,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF2E5B9A)
+            )
+            content()
+        }
+    }
+}
+
+@Composable
+fun SleepMetricsCard() {
+    MetricsCard(
+        title = "Mi sueño",
+        content = {
+            SleepInfoRow(
+                title = "Información entre 19:00 - 07:00",
+                value = "7h 36min",
+                valueColor = Color(0xFF4CAF50)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            SleepChart(
+                startHour = 22,
+                endHour = 6,
+                sleepDuration = 456
+            )
+        }
+    )
+}
+
+@Composable
+fun SleepInfoRow(
+    title: String,
+    value: String,
+    valueColor: Color
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = title,
+//            style = MaterialTheme.typography.body2,
+            color = Color.Gray
+        )
+        Text(
+            text = value,
+//            style = MaterialTheme.typography.body2,
+            color = valueColor
+        )
+    }
+}
+
+@Composable
+fun SleepChart(
+    startHour: Int,
+    endHour: Int,
+    sleepDuration: Int
+) {
+    val totalHours = endHour - startHour
+    val sleepHours = sleepDuration / 60
+    val wakeHours = totalHours - sleepHours
+
+    Box(
+        modifier = Modifier
+            .height(80.dp)
+            .fillMaxWidth()
+            .background(Color(0xFFE8F1FF), shape = RoundedCornerShape(8.dp))
+            .padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(sleepHours.toFloat() / totalHours)
+                    .fillMaxHeight()
+                    .background(Color(0xFF4285F4))
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Box(
+                modifier = Modifier
+                    .weight(wakeHours.toFloat() / totalHours)
+                    .fillMaxHeight()
+                    .background(Color(0xFFE8F1FF))
+            )
+        }
+    }
+
+}
+@Composable
+fun StepsMetricsCard() {
+    MetricsCard(
+        title = "Mis pasos",
+        content = {
+            StepsInfoRow(
+                caloriesValue = "200kcal",
+                stepsValue = "1200 pasos",
+                stepsValueColor = Color(0xFF4CAF50)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            StepsChart(
+                stepsCount = 1200
+            )
+        }
+    )
+}
+
+@Composable
+fun StepsInfoRow(
+    caloriesValue: String,
+    stepsValue: String,
+    stepsValueColor: Color
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = caloriesValue,
+//            style = MaterialTheme.typography.body2,
+            color = Color.Gray
+        )
+        Text(
+            text = stepsValue,
+//            style = MaterialTheme.typography.body2,
+            color = stepsValueColor
+        )
+    }
+}
+
+@Composable
+fun StepsChart(
+    stepsCount: Int
+) {
+    Box(
+        modifier = Modifier
+            .height(80.dp)
+            .fillMaxWidth()
+            .background(Color(0xFFE8F1FF), shape = RoundedCornerShape(8.dp))
+            .padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            repeat(5) {
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight()
+                        .background(
+                            if (it < stepsCount / 240) Color(0xFF4CAF50) else Color(
+                                0xFFE8F1FF
+                            )
+                        )
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun HeartRateMetricsCard() {
+    MetricsCard(
+        title = "Mi ritmo cardiaco",
+        content = {
+            HeartRateInfoRow(
+                lastMeasurementLabel = "Última medición registrada",
+                lastMeasurementValue = "90 LPM",
+                lastMeasurementValueColor = Color(0xFF4CAF50)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            HeartRateChart()
+        }
+    )
+}
+
+@Composable
+fun HeartRateInfoRow(
+    lastMeasurementLabel: String,
+    lastMeasurementValue: String,
+    lastMeasurementValueColor: Color
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = lastMeasurementLabel,
+//            style = MaterialTheme.typography.body2,
+            color = Color.Gray
+        )
+        Text(
+            text = lastMeasurementValue,
+//            style = MaterialTheme.typography.body2,
+            color = lastMeasurementValueColor
+        )
+    }
+}
+
+@Composable
+fun HeartRateChart(
+) {
+    Box(
+        modifier = Modifier
+            .height(80.dp)
+            .fillMaxWidth()
+            .background(Color(0xFFE8F1FF), shape = RoundedCornerShape(8.dp))
+            .padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFFF7043))
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFFF7043))
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFF4CAF50))
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFFF7043))
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun GraphicsPreview() {
+    LogifitApppTheme {
+        MainScreen1()
+    }
+}
+
+
