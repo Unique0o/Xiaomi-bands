3101995a986ec1a5b45f6533ec6dd49791e0563a
MichaelSoro <maikolsoro.z1998@gmail.com>
2024-08-14 11:36:45 -0600

    Added HomeAdmin UI

---
+++ app/src/main/java/com/example/logifitappp/ui/components/headers/TopBar.kt
@@ -0,0 +1,56 @@
+package com.example.logifitappp.ui.components.headers
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
+import com.example.logifitappp.ui.theme.Blue690
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Box(
+                modifier = modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = title,
+                    modifier = modifier.align(Alignment.Center),
+                    color = Blue690,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+
+        }, colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer
+        ),
+        actions = {
+            IconButton(onClick = { /* Acción del perfil */ }) {
+                Icon(
+                    Icons.Default.Person,
+                    contentDescription = "Perfil",
+                    tint = Blue690
+                )
+            }
+        }
+    )
+}
