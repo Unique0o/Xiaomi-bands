2d94babc26295275a75c00874ac207f339dabf25
MichaelSoro <maikolsoro.z1998@gmail.com>
2024-08-16 10:13:55 -0600

    Added AuthenticaionBandModal

--- app/src/main/java/com/example/logifitappp/ui/components/home/Device/DeviceDetectionCard.kt
+++ app/src/main/java/com/example/logifitappp/ui/components/home/Device/DeviceDetectionCard.kt
@@ -1,7 +1,9 @@
 package com.example.logifitappp.ui.components.home.Device
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material.icons.filled.Watch
 
 import androidx.compose.material3.*
@@ -9,8 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.example.logifitappp.R
 import com.example.logifitappp.ui.theme.Blue690
 import com.example.logifitappp.ui.theme.Orange390
 
@@ -77,13 +81,19 @@ fun InfoMessage(
 }
 
 @Composable
-fun DeviceCard(deviceName: String, deviceId: String) {
+fun DeviceCard(
+    deviceName: String,
+    deviceId: String,
+    onClick: () -> Unit
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp),
+            .padding(vertical = 4.dp)
+            .clickable(onClick = onClick),
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainer)
+
     ) {
         Row(
             modifier = Modifier
@@ -91,16 +101,35 @@ fun DeviceCard(deviceName: String, deviceId: String) {
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(
-                imageVector = Icons.Default.Watch,
-                contentDescription = null,
-                modifier = Modifier.size(24.dp)
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            Column {
-                Text(text = deviceName, style = MaterialTheme.typography.bodyLarge)
-                Text(text = deviceId, style = MaterialTheme.typography.bodyMedium)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.weight(1f)
+                ) {
+                Icon(
+                    imageVector = Icons.Default.Watch,
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Column {
+                    Text(
+                        text = deviceName,
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                    Text(
+                        text = deviceId,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
             }
+            Icon(
+                imageVector = Icons.Default.ChevronRight,
+                contentDescription = stringResource(id = R.string.device_card_description),
+                tint = MaterialTheme.colorScheme.onSurfaceVariant
+            )
         }
     }
 }
---
+++ app/src/main/java/com/example/logifitappp/ui/components/modals/AuthenticationModal.kt
@@ -0,0 +1,122 @@
+package com.example.logifitappp.ui.components.modals
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.example.logifitappp.R
+import com.example.logifitappp.ui.components.forms.OutlinedTextField
+
+
+@Composable
+fun AuthenticationModal(
+    isVisible: Boolean,
+    onDismiss: () -> Unit,
+    onAuthenticate: (String) -> Unit
+) {
+    var authKey by remember { mutableStateOf("0x") }
+
+    if (isVisible) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.4f))
+                .clickable(onClick = onDismiss)
+        ) {
+            Card(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clickable { },
+                shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            stringResource(R.string.authentication_title),
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                        IconButton(onClick = onDismiss) {
+                            Icon(
+                                Icons.Default.Close,
+                                contentDescription = stringResource(id = R.string.close)
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        stringResource(id = R.string.authentication_description),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+//                    OutlinedTextField(
+//                        value = authKey,
+//                        onValueChange = { authKey = it },
+//                        label = { Text(stringResource(id = R.string.enter_key)) },
+//                        leadingIcon = { Text("0x") },
+//                        modifier = Modifier.fillMaxWidth()
+//                    )
+                    OutlinedTextField(
+                        value = TextFieldValue(authKey),
+                        onValueChange = {
+                            if (it.text.startsWith("0x")) {
+                                authKey = it.text
+                            }
+                        },
+                        placeholder = stringResource(id = R.string.enter_key),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(
+                        onClick = { onAuthenticate(authKey) },
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = if (authKey.length > 2) MaterialTheme.colorScheme.primary else Color.Gray
+                        ),
+                        enabled = authKey.length > 2
+                    ) {
+                        Text(stringResource(id = R.string.authentication_title))
+                    }
+                }
+            }
+        }
+    }
+}
