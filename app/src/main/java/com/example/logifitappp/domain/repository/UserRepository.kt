f5554b28d9b454d9e6ed26b29151b8bedf5304c4
michcastrillo <michcastrillo63@gmail.com>
2024-10-25 16:56:27 -0600

    photo additional data

--- app/src/main/java/com/example/logifitappp/domain/repository/UserRepository.kt
+++ app/src/main/java/com/example/logifitappp/domain/repository/UserRepository.kt
@@ -3,6 +3,7 @@ package com.example.logifitappp.domain.repository
 import com.example.logifitappp.data.models.UserModel
 import com.example.logifitappp.data.remote.dto.requests.StorePersonalInformationRequest
 import com.example.logifitappp.data.remote.dto.response.StorePersonalInformationResponse
+import okhttp3.MultipartBody
 import retrofit2.Response
 
 interface UserRepository {
@@ -13,4 +14,6 @@ interface UserRepository {
     suspend fun store(userId: Int, request: StorePersonalInformationRequest): Response<StorePersonalInformationResponse>
 
     suspend fun getLoggedIn(): UserModel?
+
+    suspend fun uploadProfilePhoto(userId: String, image: MultipartBody.Part): Response<Unit>
 }
--- app/src/main/java/com/example/logifitappp/domain/service/UserService.kt
+++ app/src/main/java/com/example/logifitappp/domain/service/UserService.kt
@@ -1,14 +1,22 @@
 package com.example.logifitappp.domain.service
 
 
+import android.content.Context
+import android.net.Uri
 import com.example.logifitappp.data.remote.dto.requests.StorePersonalInformationRequest
 import com.example.logifitappp.domain.repository.UserRepository
 import com.example.logifitappp.enums.AppStatusCodeEnum
 import com.example.logifitappp.exceptions.HttpConsumerException
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import retrofit2.Response
+import java.io.File
 import javax.inject.Inject
 
 class UserService @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val context: Context
 ) {
     suspend fun storePersonalInformation(
         userId: Int,
@@ -29,5 +37,20 @@ class UserService @Inject constructor(
                 else -> throw HttpConsumerException(AppStatusCodeEnum.NO_INTERNET_CONNECTION)
             }
         }
+    }
+
+    suspend fun updateProfilePhoto(userId: String, uri: Uri): Response<Unit> {
+        val inputStream = context.contentResolver.openInputStream(uri)
+        val file = File(context.cacheDir, "temp_profile_photo.jpg")
+
+        inputStream?.use { input ->
+            file.outputStream().use { output ->
+                input.copyTo(output)
+            }
+        }
+        val requestFile = file.asRequestBody("image/*".toMediaTypeOrNull())
+        val imagePart = MultipartBody.Part.createFormData("image", file.name, requestFile)
+
+        return userRepository.uploadProfilePhoto(userId, imagePart)
     }
 }
