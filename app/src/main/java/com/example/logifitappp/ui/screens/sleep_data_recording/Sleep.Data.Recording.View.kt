92a2ae170ed833d4dcb441a9d2d9cae7a97450b5
@J.Richard <richard.tejedo@outlook.com>
2024-11-25 18:50:40 -0500

    feat: merge with sleep data recording

--- app/src/main/java/com/example/logifitappp/ui/screens/sleep_data_recording/Sleep.Data.Recording.View.kt
+++ app/src/main/java/com/example/logifitappp/ui/screens/sleep_data_recording/Sleep.Data.Recording.View.kt
@@ -4,11 +4,6 @@ package com.example.logifitappp.ui.screens.sleep_data_recording
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
